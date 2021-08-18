02e839353bf434bc1854dfa654e82f84cb3006f1
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-08-18 14:26:01 +0530

    Express Sessions Part 2

--- app.js
+++ app.js
@@ -36,41 +36,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("Your are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === "admin" && password === "password") {
-      req.session.user = "admin";
-      next();
-    }
-    else {
-      var err = new Error("Your are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-
-    }
-
-
+    var err = new Error("Your are not authenticated!");
+    err.status = 401;
+    return next(err);
   }
   else {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     }
     else {
       var err = new Error("You are not authenticated!");
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -79,8 +61,7 @@ function auth(req, res, next) {
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
