051c303839a98b1838cff60bf130963028dfbfc0
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-08-27 10:42:29 +0530

    passport-facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey':"12345-7890-09876-54321",
-    "mongoUrl":'mongodb://localhost:27017/conFusion'
+    'secretKey': "12345-7890-09876-54321",
+    "mongoUrl": 'mongodb://localhost:27017/conFusion',
+    "facebook": {
+        clientId: "1278911302558920",
+        clientSecret: "478625a61e98556c62e4208a8798ead9"
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -70,4 +70,14 @@ router.get("/logout", cors.corsWithOptions, (req, res) => {
     next(err);
   }
 });
+
+router.get("/facebook/token", passport.authenticate("facebook-token"), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({ success: true, token: token, status: "You are Successfully logged in!" })
+  }
+})
+
 module.exports = router;
