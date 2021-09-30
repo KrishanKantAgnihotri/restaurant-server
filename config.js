b0ec9dea83f3c489816a81e84567fec16c547e23
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-09-30 16:21:28 +0530

    .

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': "12345-7890-09876-54321",
-    "mongoUrl": 'mongodb+srv://kk2_agnihotri:KRISHNa2@mern-restaurant.vlmly.mongodb.net/test',
+    "mongoUrl": 'mongodb+srv://kk2_agnihotri:KRISHNa2@mern-restaurant.vlmly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     "facebook": {
         clientId: "1278911302558920",
         clientSecret: "478625a61e98556c62e4208a8798ead9"
