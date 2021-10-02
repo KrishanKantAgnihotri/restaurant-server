678e31e2cacc8a623881672fa62b7f4d20a2e676
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2021-10-02 09:22:04 +0530

    Update uploadRouter.js

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -31,23 +31,23 @@ const uploadRouter = express.Router();
 uploadRouter.use(bodyParser.json());
 
 uploadRouter.route("/")
-    .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-    .get(cors.cors, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    .options( (req, res) => { res.sendStatus(200); })
+    .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         res.statusCode = 403;
         res.end("GET operation not supported on /imageUpload")
 
     })
-    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    .put( authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         res.statusCode = 403;
         res.end("PUT operation not supported on /imageUpload")
 
     })
-    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, upload.single("imageFile"), (req, res, next) => {
+    .post( authenticate.verifyUser, authenticate.verifyAdmin, upload.single("imageFile"), (req, res, next) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
         res.json(req.file);
     })
-    .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    .delete( authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         res.statusCode = 403;
         res.end("DELETE operation not supported on /imageUpload")
 
