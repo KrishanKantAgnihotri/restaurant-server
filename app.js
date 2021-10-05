8cfef58e187db22d1de23e59acbfdb101d7fdb67
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2021-10-05 13:05:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,12 +30,7 @@ connect.then((db) => {
 var app = express();
 
 app.all("*", (req, res, next) => {
-  if (req.secure) {
     return next();
-  }
-  else {
-    res.redirect(307, "https://" + req.hostname + ":" + app.get("secPort") + req.url);
-  }
 
 });
 // view engine setup
